7f6d6aa09d51a41ad28eb28fbc796c80a0c9405f
rickstrahl <rstrahl@west-wind.com>
2019-11-19 00:38:33 -1000

    At Watch scripts using LiveReload Server (dotnetcore sdk required)

---
+++ watch.sh
@@ -0,0 +1,11 @@
+# This script launches a local Web Server that includes Live Reload functionality
+# and navigates to the sample page.
+
+# Requires the Dotnet Core 3.0+ SDK on Windows, Mac or Linux 
+# To install:
+#
+# dotnet tool install -g LiveReloadServer  
+
+
+x-www-browser 'http://localhost:5200/sample/'
+LiveReloadServer --OpenBrowser false
